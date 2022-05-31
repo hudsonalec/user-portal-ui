65abe5badb41176c325d70e437bc917a6ad5ddc4
hudsonalec <99743107+hudsonalec@users.noreply.github.com>
2022-05-31 11:02:02 -0500

    changes added

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,3 +1,4 @@
+import { UserService } from './../../services/user.service';
 import { Address } from './../../models/user';
 import { Component, OnInit } from '@angular/core';
 import { User } from 'src/app/models/user';
@@ -13,21 +14,30 @@ export class RegisterComponent {
   title = 'Register User';
   public user = new User(0, '', '', '', '', '', []);
   public address = new Address('', '', '', '', '');
-  clientMessage = new ClientMessge('');
+  public clientMessage = new ClientMessge('');
 
 
   // Step 1. Capture the user info from the form, and build a user object
 
   // Step 2. Send a POST request to the Spring Boot API with the user object in the Request Body
-  // ( We need HttpService for this)
+  // ( We need HttpService for this which is in turn injected into the UserService)
 
-  constructor() { }
+  constructor(private userService: UserService) { }
 
   public registerUser(): void {
 
     // call upon a service to transmit the object via a HTTP to a backend API
+    this.user.addresses.push(this.address);
+
+    // send the user object to Spring Boot to persist in the db
+    this.userService.registerUser(this.user)
+      .subscribe(data => console.log(data), error => console.error(error));
+      // first function is what to do with successful invocation
+      // second function is what
   }
 
 
 
 }
+
+
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,6 +1,8 @@
+import { User } from './../models/user';
 import { url } from './../../environments/environment'; // imports the URL that points to localhost:5000/api
 import { Injectable } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpErrorResponse, HttpHeaders } from '@angular/common/http';
+import { catchError, Observable, throwError } from 'rxjs';
 
 
 // This UserService is entirely responsible for making HTTP requests to the UserController
@@ -12,12 +14,36 @@ const userUrl = url + '/users'; // now the url is pointing to http://localhost:5
 })
 export class UserService {
 
+  // these are the headers that we append to every request to the server
+  httpOptions = {
+    headers: new HttpHeaders({'Content-Type' : 'application/json'})
+  }
+
   // inject ANOTHER service into this service
   // first add HttpClient Module to your app.module.ts file
   constructor(private http: HttpClient) { }
 
-  registerUser() {
-
+  // POST request to our Spring Boot API
+  registerUser(user: User): Observable<User> {
+    //3 params: url, object in request body, options (headers)
+    return this.http.post<User>(`${userUrl}/add`, user, this.httpOptions)
+      .pipe(catchError(this.handleError)); // pass a callback function if something goes wrong
+      // catchError comes from rxjs
 
   }
+
+  // this method is private so we can call it in the case that something goes wrong
+  private handleError(httpError: HttpErrorResponse) {
+
+    if (httpError.error instanceof ErrorEvent) {
+      console.log('An error occured: ', httpError.error.message)
+    } else {
+      console.error(`
+      Backend returned code ${httpError.status}
+      body was: ${httpError.error}`)
+    }
+
+    return throwError(()=> Error('something redally bad happened, please try again later'))
+  }
+
 }
